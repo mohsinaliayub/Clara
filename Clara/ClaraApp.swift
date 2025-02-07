c9aff6b614aa991f2b2462020a944e03aca3525d
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-02-07 22:55:44 +0100

    Initial Commit

---
+++ Clara/ClaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaraApp.swift
+//  Clara
+//
+//  Created by Mohsin Ali Ayub on 07.02.25.
+//
+
+import SwiftUI
+
+@main
+struct ClaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clara/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clara
+//
+//  Created by Mohsin Ali Ayub on 07.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
